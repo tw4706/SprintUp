dd0cfd649c2e07f4ea4cc4cad349e3b739ac5ce6
tw4706 <towajobseach@gmail.com>
2025-09-12 18:32:16 +0900

    up

---
+++ SprintUp/Assets/Oosaki/GameStartManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour
+{
+    // ボタン
+    public Button _button;
+    // シーン遷移先
+    public string scene;
+    // スタートしているかどうか
+    bool isStart = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_button==null)
+        {
+            _button = GetComponent<Button>();
+        }
+        // ボタンがクリックされた際に呼び出される関数
+        _button.onClick.AddListener(OnButtonClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void OnButtonClick()
+    {
+        Debug.Log("ボタンを押した");
+        SceneManager.LoadScene(scene);
+    }
+}
