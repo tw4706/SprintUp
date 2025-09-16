347443d12519c51ddda971e9c71295b51a1de615
kudousyota <fko2547040@stu.o-hara.ac.jp>
2025-09-16 18:49:30 +0900

    足場追加

--- SprintUp/Assets/kudo/kudo player.cs
+++ SprintUp/Assets/kudo/kudo player.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 public class kudoplayer : MonoBehaviour
 {
     float Speed = 3.0f;
+    public float JumpPower = 2.0f;
 
+    Rigidbody rb;
     // Start is called before the first frame update
-    
-      
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
 
-        // Update is called once per frame
-        void Update()
+
+    // Update is called once per frame
+    void Update()
         {
             if (Input.GetKey(KeyCode.W))
             {
@@ -30,7 +35,10 @@ public class kudoplayer : MonoBehaviour
             {
                 transform.position -= Speed * transform.right * Time.deltaTime;
             }
-
-        }
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(transform.up * JumpPower);
+            }
+    }
     }
 
