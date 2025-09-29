a7165de0331e4f7fcba72b7544144ac3f10adb1c
Tiger5286 <toratiger5286@gmail.com>
2025-09-29 18:22:49 +0900

    プレイヤーのジャンプアニメーション実装

--- SprintUp/Assets/ToraTiger/textures/Player/PlayerAnimation.cs
+++ SprintUp/Assets/ToraTiger/textures/Player/PlayerAnimation.cs
@@ -15,24 +15,27 @@ public class PlayerAnimation : MonoBehaviour
 
     void Update()
     {
-        if (playerMove.animationType == 0)
+        if (playerMove.isFalling)
         {
-            animator.SetBool("isJog", false);
-            animator.SetBool("isDash", false);
+            animator.SetBool("isFalling", true);
         }
-        else if (playerMove.animationType == 1)
+        else
         {
-            animator.SetBool("isJog", true);
-            animator.SetBool("isDash", false);
-        }
-        else if (playerMove.animationType == 2)
-        {
-            animator.SetBool("isDash", true);
-        }
-        else if (playerMove.animationType == 3)
-        {
-            animator.SetTrigger("Jump");
-            playerMove.animationType = 0;  // ジャンプアニメーションが終わったらidleに戻す
+            animator.SetBool("isFalling", false);
+            if (playerMove.animationType == 0)
+            {
+                animator.SetBool("isJog", false);
+                animator.SetBool("isDash", false);
+            }
+            else if (playerMove.animationType == 1)
+            {
+                animator.SetBool("isJog", true);
+                animator.SetBool("isDash", false);
+            }
+            else if (playerMove.animationType == 2)
+            {
+                animator.SetBool("isDash", true);
+            }
         }
     }
 }
