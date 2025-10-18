5966ee509bb1e571cfab84d880c300460e290cec
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2025-10-18 11:47:46 +0900

    ボタンを押す音追加

--- SprintUp/Assets/Oosaki/Script/SceneChanger.cs
+++ SprintUp/Assets/Oosaki/Script/SceneChanger.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 public class SceneChanger : MonoBehaviour
 {
     public FadeManager fadeManager;
+    AudioSource audioSource;
+    public AudioClip PressButtonSound;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void OnClickChangeScene(string sceneName)
     {
+        audioSource.PlayOneShot(PressButtonSound);
         fadeManager.ChangeScene(sceneName);
     }
 }
